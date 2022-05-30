f604bb6dda0134ddae4afd6a0021a7f9458c05e9
Peter Freese <pfreese@grailbio.com>
2022-05-30 09:50:11 -0700

    counting_sort

---
+++ template_sort/counting_sort.py
@@ -0,0 +1,41 @@
+import sort_utils
+
+
+def countingSort(array):
+
+    if len(array) < 2:
+        return array
+
+    assert all([isinstance(x, int) for x in array])
+
+    minVal = min(array)
+    maxVal = max(array)
+
+    valRange = maxVal - minVal
+
+    counts = [0 for _ in range(valRange + 1)]
+
+    for v in array:
+        counts[v - minVal] += 1
+    # Get cumsum of counts.
+    for i in range(1, len(counts)):
+        counts[i] = counts[i] + counts[i-1]
+
+    result = [None for _ in range(len(array))]
+    for val in array:
+        countsIdx = val - minVal
+        resultIdx = counts[countsIdx] - 1
+        counts[countsIdx] -= 1
+        result[resultIdx] = val
+    return result
+
+
+
+# TESTS
+
+intsActual = countingSort(sort_utils.unsortedInts)
+assert intsActual == sort_utils.sortedInts
+print(intsActual)
+
+assert countingSort([]) == []
+assert countingSort([2]) == [2]
