9baebf850ee301457fee3bcd38eaeebe0ce7d060
Peter Freese <pfreese@grailbio.com>
2022-07-10 20:40:57 -1000

    add array syntax update

--- template_sort/array_syntax.py
+++ template_sort/array_syntax.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 test_ls = [1, 1, 3, 4, 9, 10]
 
 ## REDUCE
@@ -8,7 +10,6 @@ def reduce_mult(lst):
 
 
 def reduce_concat(lst):
-
 
 # Tests for REDUCE
 assert reduce_add(test_ls) == sum(test_ls)
@@ -31,3 +32,23 @@ def filter_even(lst):
 
 
 assert filter_even(test_ls) == [4, 10]
+
+# LOOP LOGIC.
+# CBP (continue; break; pass)
+# (Customs and Border Patrol)
+
+# Print 1-10 except for 5.
+result = []
+
+assert result == [0, 1, 2, 3, 4, 6, 7, 8, 9]
+
+# Two loops each 0, 1, 2; break inner loop once it hits 1.
+result = []
+
+assert result == ['i=0, j=0', 'i=1, j=0', 'i=2, j=0']
+
+
+# Pass: skip over x=5 but print all 0-10.
+result = []
+
+assert result == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
